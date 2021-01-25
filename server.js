cfc9d2324ff70ac288c9fa85398992923a4153e6
GoncaloNevesCorreia <57664115+GoncaloNevesCorreia@users.noreply.github.com>
2021-01-25 22:46:34 +0000

    My first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const sockets = new Server(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 const untakenSpace = 0;
